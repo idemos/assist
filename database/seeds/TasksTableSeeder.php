5d403e3834899c2e0887578bf9060acd87d884d2
idemos <cirogiametta@msn.com>
2020-02-10 10:03:14 +0100

    corrected seeds

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class TasksTableSeeder extends Seeder
@@ -11,14 +12,22 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-		DB::table('tasks')->insert([
-            'name' => 'Microsoft Office License',
-        ],[
-            'name' => 'Email Access Granted',
-        ],[
-            'name' => 'Git Repository Granted',
-        ],[
-            'name' => 'JiraAccess Granted',
-        ]);
+		DB::table('tasks')->insert([[
+                    'name' => 'Microsoft Office License',
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ],[
+                    'name' => 'Email Access Granted',
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ],[
+                    'name' => 'Git Repository Granted',
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ],[
+                    'name' => 'JiraAccess Granted',
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ]]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,15 +14,21 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'type' => '1',
-            'password' => Hash::make('admin'),
-        ],[
-            'name' => 'user',
-            'email' => 'user@user.com',
-            'type' => '0',
-            'password' => Hash::make('user'),
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'type' => '1',
+                'password' => Hash::make('admin'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],[
+                'name' => 'user',
+                'email' => 'user@user.com',
+                'type' => '0',
+                'password' => Hash::make('user'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]
         ]);
     }
 }
